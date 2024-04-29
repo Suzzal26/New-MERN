406fc033c30920ef36a7d15813de79ce0ae97133
Suzzal26 <suzzalghimire@gmail.com>
2024-04-29 16:43:00 +0545

    made changes

---
+++ variables.js
@@ -0,0 +1,7 @@
+//Write a js program that ask user to enter their dob and calculate their age
+
+const dob=Number(prompt("Write your date of birth"));
+const age=2024-dob;
+alert(age);
+console.log(dob)
+
